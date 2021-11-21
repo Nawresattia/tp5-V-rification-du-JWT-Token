3b93e3387e67cc79ef4b5105bae3fdf7305a2ae2
Nawresattia <attianawres37@gmail.com>
2021-11-21 14:35:29 +0100

    first commit

---
+++ src/main/java/com/nawres/users/service/UserService.java
@@ -0,0 +1,11 @@
+package com.nawres.users.service;
+import com.nawres.users.entities.Role;
+import com.nawres.users.entities.User;
+public interface UserService {
+	
+	User saveUser(User user);
+	User findUserByUsername (String username);
+	Role addRole(Role role);
+	User addRoleToUser(String username, String rolename);
+
+}
